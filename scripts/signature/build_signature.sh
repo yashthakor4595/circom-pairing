de4850fece1292ac052e3c0ef8c44efdb6bfa5af
yashthakor <yashthakor4595@gmail.com>
2023-06-16 13:40:23 +0530

    changes in path

--- scripts/signature/build_signature.sh
+++ scripts/signature/build_signature.sh
@@ -39,40 +39,40 @@ echo "DONE ($((end-start))s)"
 #echo "DONE ($((end-start))s)"
 
 cd ..
-snarkjs wej witness.wtns witness.json
+/home/ubuntu/snarkjs/cli.js wej witness.wtns witness.json
 
 echo "****GENERATING ZKEY 0****"
 start=`date +%s`
-/data/node/out/Release/node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /data/snarkjs/cli.js zkey new "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME"_0.zkey -v > zkey0.out
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/snarkjs/cli.js zkey new "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME"_0.zkey -v > zkey0.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
 start=`date +%s`
-/data/node/out/Release/node /data/snarkjs/cli.js zkey contribute -verbose "$CIRCUIT_NAME"_0.zkey "$CIRCUIT_NAME".zkey -n="First phase2 contribution" -e="some random text 5555" > contribute.out
+node /home/ubuntu/snarkjs/cli.js zkey contribute -verbose "$CIRCUIT_NAME"_0.zkey "$CIRCUIT_NAME".zkey -n="First phase2 contribution" -e="some random text 5555" > contribute.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****VERIFYING FINAL ZKEY****"
 start=`date +%s`
-/data/node/out/Release/node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /data/snarkjs/cli.js zkey verify -verbose "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME".zkey > verify.out
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/snarkjs/cli.js zkey verify -verbose "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME".zkey > verify.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****EXPORTING VKEY****"
 start=`date +%s`
-/data/node/out/Release/node /data/snarkjs/cli.js zkey export verificationkey "$CIRCUIT_NAME".zkey vkey.json -v
+node /home/ubuntu/snarkjs/cli.js zkey export verificationkey "$CIRCUIT_NAME".zkey vkey.json -v
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****GENERATING PROOF FOR SAMPLE INPUT****"
 start=`date +%s`
-/data/rapidsnark/build/prover "$CIRCUIT_NAME".zkey witness.wtns proof.json public.json > proof.out
+/home/ubuntu/rapidsnark/build/prover "$CIRCUIT_NAME".zkey witness.wtns proof.json public.json > proof.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****VERIFYING PROOF FOR SAMPLE INPUT****"
 start=`date +%s`
-/data/node/out/Release/node /data/snarkjs/cli.js groth16 verify vkey.json public.json proof.json -v
+node /home/ubuntu/snarkjs/cli.js groth16 verify vkey.json public.json proof.json -v
 end=`date +%s`
 echo "DONE ($((end-start))s)"
